ff40e1370bb1734e2f9609e4c23ecb20a3754bdb
Hussain-Aqeel <hussain@ljassim.tech>
2023-02-06 12:16:16 +0300

    migrations and few seeders 2

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CommentSeeder extends Seeder
 {
@@ -14,6 +15,67 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('comments')->insert([
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 1,
+            'recipe_id' => 1
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 1
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 2
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 3
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 1,
+            'recipe_id' => 4
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 5
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 5
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 6
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 3,
+            'recipe_id' => 7
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 8
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 2,
+            'recipe_id' => 9
+          ],
+          [
+            'content' => ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tempor magna vitae quam sollicitudin, sed tristique metus commodo.',
+            'user_id' => 1,
+            'recipe_id' => 10
+          ],
+        ]);
     }
 }
